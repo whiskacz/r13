df89a0868f6167b416cc8de73808c896a82abc56
whiskacz <pawelblasiak1985@gmail.com>
2023-07-16 21:16:06 +0200

    React practice r13

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect } from "react";
+import ReactDOM from 'react-dom/client'
+import './css/index.css'
+import { LiaLaughBeamSolid, LiaMehSolid } from "react-icons/lia";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default function R13App () {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    const [status, setStatus] = useState({
+        status: null,
+        description: "Hello buddy, would You like to subscribe?"
+    })
+
+    const handleToSubChange = () => {
+        setStatus({
+            status : "subscribe",
+            description: "description done :D"
+        })
+    }
+
+    const handleToUnSubChange = () => {
+        setStatus ({
+            status : "unsubscribe",
+            description: "You already unsubscribe"
+        })
+    }
+
+    useEffect(()=> {
+        console.log(status)
+    },[status])
+
+
+   return (
+    <div className="wrapper">
+            <div className="container">
+                <span>{status.description}</span>
+                {status.status === "subscribe" && <LiaLaughBeamSolid/>}
+                {status.status === "unsubscribe" && <LiaMehSolid/>}
+
+            </div>
+            <div className="container">
+                {(status.status === null) && <button onClick={handleToUnSubChange}>Cancel</button>}
+                {status.status === "subscribe" && <button  onClick={handleToUnSubChange}>Unsubscribe</button>}
+                {(status.status === null || "unsubscribe") && <button className="sub" onClick={handleToSubChange}>Subscribe</button>}
+        </div>
+    </div>
+)}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<R13App />)
